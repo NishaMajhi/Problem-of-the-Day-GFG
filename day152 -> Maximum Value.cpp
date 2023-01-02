b514c4e0e29ac36400d2387e81d603023d2b7f06
Nisha Majhi <84006994+NishaMajhi@users.noreply.github.com>
2023-01-02 10:52:23 +0530

    Create day152 -> Maximum Value.cpp

---
+++ day152 -> Maximum Value.cpp
@@ -0,0 +1,89 @@
+/*
+Given a binary tree, find the largest value in each level.
+
+
+Example 1:
+Input:
+        1
+       / \
+      2   3 
+Output:
+1 3
+Explanation:
+At 0 level, values of nodes are {1}
+                 Maximum value is 1
+At 1 level, values of nodes are {2,3}
+Maximum value is 3
+
+
+
+Example 2:
+Input:
+        4
+       / \
+      9   2
+     / \   \
+    3   5   7 
+Output:
+4 9 7
+Explanation:
+At 0 level, values of nodes are {4}
+                 Maximum value is 4
+At 1 level, values of nodes are {9,2}
+                 Maximum value is 9
+At 2 level, values of nodes are {3,5,7}
+                 Maximum value is 7
+ 
+
+Your Task:
+You don't need to read input or print anything.Your task is to complete the function maximumValue() that takes root node as input parameter
+and returns a list of integers containing the maximum value at each level. 
+The size of the resultant list should be equal to the height of the binary tree and result[i] should store the maximum value at level i.
+
+
+Expected Time Complexity: O(N), where N is the number of nodes.
+Expected Auxiliary Space: O(H), where H is the height of binary tree.
+
+
+Constraints:
+1 ≤ Number of nodes ≤ 10^4
+1 ≤ Data of a node ≤ 10^5
+*/
+
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+}; */
+
+class Solution {
+  public:
+    void dfs(map<int,int>&mp,Node *node,int level){
+        if(node==NULL)
+          return;
+        if(mp[level]!=0){
+            if(node->data>mp[level]){
+               mp[level]=node->data;
+            }
+        }
+        else
+           mp[level]=node->data;
+           
+        dfs(mp,node->left,level+1);
+        dfs(mp,node->right,level+1);
+      
+    }
+    vector<int> maximumValue(Node* node) {
+        //code here
+        map<int,int>mp;
+        dfs(mp,node,0);
+        vector<int>ans;
+        for(auto itr:mp){
+            ans.push_back(itr.second);
+        }
+        
+        return ans;
+    }
+};
